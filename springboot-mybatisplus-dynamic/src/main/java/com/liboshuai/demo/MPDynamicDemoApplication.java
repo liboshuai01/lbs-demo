24c6db945b221b45526c3c171e830e60a55eedff
Boshuai Li <liboshuai01@gmail.com>
2025-07-11 17:33:50 +0800

    feat(springboot): 添加 MyBatis Plus 动态数据源模块
    - 新增 springboot-mybatisplus-dynamic 模块
    - 实现了基于 MyBatis Plus 的动态数据源配置
    - 添加了用户实体、映射和控制器
    - 配置了分页插件和日志

---
+++ springboot-mybatisplus-dynamic/src/main/java/com/liboshuai/demo/MPDynamicDemoApplication.java
@@ -0,0 +1,11 @@
+package com.liboshuai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPDynamicDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MPDynamicDemoApplication.class, args);
+    }
+}
