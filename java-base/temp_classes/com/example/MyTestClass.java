1f1163de164daa14857f59764f7045a410784fda
Boshuai Li <liboshuai01@gmail.com>
2025-11-14 03:17:33 +0800

    add jvm

---
+++ java-base/temp_classes/com/example/MyTestClass.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class MyTestClass {
+
+    public void sayHello() {
+        System.out.println("==============================================");
+        System.out.println("[MyTestClass]: Hello! World");
+        System.out.println("==============================================");
+    }
+}
