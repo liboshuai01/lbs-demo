dae4edad02fef738d122bc35050d7b4182c51ad0
Boshuai Li <liboshuai01@gmail.com>
2026-01-15 20:39:50 +0800

    feat(blog-tool): 新增博客工具
    wiki.js转hexo博客的md文件工具

--- hexo2wiki-tool/src/main/java/com/liboshuai/demo/service/ConversionService.java
+++
@@ -1,7 +0,0 @@
-package com.liboshuai.demo.service;
-
-import java.io.IOException;
-
-public interface ConversionService {
-    long processDirectory(String sourceDirectoryPath, String targetDirectoryPath) throws IOException;
-}
